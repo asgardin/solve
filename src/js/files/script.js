4ba131675288e9587449e19c72cd643daa8103b7
Niko <odasgardin@gmail.com>
2023-03-28 11:08:08 +0200

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,7 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+// Анимация Контента
+import AOS from 'aos';
+AOS.init();
